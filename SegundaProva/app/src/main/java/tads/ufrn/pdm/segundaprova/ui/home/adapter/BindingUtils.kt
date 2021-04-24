7726f1145c2e3e80cc821270f98066da5ad89fd3
Anderson <andersonlundberg@gmail.com>
2021-04-24 17:40:13 -0300

    melhoria em comida_inflater e criacao de bidingutils

---
+++ SegundaProva/app/src/main/java/tads/ufrn/pdm/segundaprova/ui/home/adapter/BindingUtils.kt
@@ -0,0 +1,8 @@
+package tads.ufrn.pdm.segundaprova.ui.home.adapter
+
+import androidx.databinding.BindingAdapter
+
+/*
+* Não houve necessidade de criar as funções. Deixo aqui registrada a existência de bindinutils. =D
+* ALH
+* */
--- SegundaProva/app/src/main/java/tads/ufrn/pdm/segundaprova/ui/home/adapter/ComidaAdapter.kt
+++ SegundaProva/app/src/main/java/tads/ufrn/pdm/segundaprova/ui/home/adapter/ComidaAdapter.kt
@@ -1,13 +1,14 @@
 package tads.ufrn.pdm.segundaprova.ui.home.adapter
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import tads.ufrn.pdm.segundaprova.R
+import tads.ufrn.pdm.segundaprova.databinding.ComidaInflaterBinding
 import tads.ufrn.pdm.segundaprova.model.Comida
 
 class ComidaAdapter: ListAdapter<Comida, ComidaAdapter.ComidaViewHolder>(ComidaDiffCallBack()) {
@@ -21,21 +22,17 @@ class ComidaAdapter: ListAdapter<Comida, ComidaAdapter.ComidaViewHolder>(ComidaD
         holder.bind(comidaescolhida)
     }
 
-    class ComidaViewHolder private constructor(v: View) : RecyclerView.ViewHolder(v) {
-        val textViewNomeComida: TextView
-        init {
-            textViewNomeComida = v.findViewById(R.id.nomeComidaTextView)
-        }
+    class ComidaViewHolder private constructor(val binding: ComidaInflaterBinding) : RecyclerView.ViewHolder(binding.root) {
 
         fun bind(comidaescolhida: Comida) {
-            textViewNomeComida.text = comidaescolhida.nomeComida
-            //img.setImageResource(comidaescolhida.img)
+            binding.comida = comidaescolhida
         }
 
         companion object {
             fun from(parent: ViewGroup): ComidaViewHolder {
-                val view = LayoutInflater.from(parent.context).inflate(R.layout.comida_inflater, parent, false)
-                return ComidaViewHolder(view)
+                val inflater = LayoutInflater.from(parent.context)
+                val binding:ComidaInflaterBinding = DataBindingUtil.inflate(inflater,R.layout.comida_inflater,parent,false)
+                return ComidaViewHolder(binding)
             }
         }
     }
